a6afaa38ccf5ce3d13f251efbda385c150582803
jessloukou85 <loukoujess85@gmail.com>
2022-07-25 17:02:13 +0100

    debut

---
+++ f_bootstrap/index.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+      .box{
+        width: 250px;
+        height: 250px;
+      }
+    </style>
+        <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <title>formation_bootstrap</title>
+  </head>
+  <body>
+  <!--  <h1>Hello, world!</h1>
+  <div class="container">
+    <p class="bg-info display-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque consequatur </p><p><small class="text-muted">omnis laborum aperiam ad vero excepturi eum, voluptas, reprehenderit dolorem rerum, eos facere voluptate est voluptatem nobis quae delectus.</small></p>
+    <button class="btn btn-success btn-lg">sauvegarder</button>
+    <a href="http://www.abidjan.net" class="btn btn-secondary btn-sm" role="button" >voir abidjan</a>
+    <a href="http://www.abidjan.net" class="btn btn-secondary btn-lg" role="button" >voir abidjan</a>
+    <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo voluptatibus quam mollitia iste dolore odio laborum temporibus, quis tempore deserunt.</p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, dolor.
+  </div>
+
+  
+
+<div class="jumbotron jumbotron-fluid">
+<div class="container">
+    <h1 class="display-4">Hello, world!</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <a class="btn btn-success btn-lg" href="#" role="button">Learn more</a>
+  </div>
+</div>
+-->
+      
+       <!--  <ul class="nav">
+           <li class="nav-item"><a href="#" class="nav-link">link</a></li>
+           <li class="nav-item"><a href="#" class="nav-link">link</a></li>
+           <li class="nav-item"><a href="#" class="nav-link">link</a></li>
+          </ul>
+          <ul class="nav nav-tabs"> 
+            <li class="nav-item"><a href="#" class="nav-link active">link</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">link</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">link</a></li>
+          </ul>
+          <ul class="nav nav-pills"> 
+            <li class="nav-item"><a href="#" class="nav-link active">link</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">link</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">link</a></li>
+          </ul>
+          <ul class="nav justify-content-around justify-content-sm-center justify-content-md-end"> 
+            <li class="nav-item"><a href="#" class="nav-link">link</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">link</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">link</a></li>
+          </ul>
+        -->
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    loukou
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav-tabs">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="container container-muted"> 
+  <form>
+    <div class="form-row">
+      <div class="form-group col-xs-4 col-sm-4 col-md-6 bg-info">
+        <label for="inputEmail4">Email</label>
+        <input type="email" class="form-control" id="inputEmail4">
+      </div>
+      <div class="form-group col-xs-4 col-sm-4 col-md-6 bg-danger">
+        <label for="inputPassword4">Password</label>
+        <input type="password" class="form-control-sm" id="inputPassword4">
+      </div>
+      <div class="form-group col-xs-4 col-sm-4 col-md-6 bg-success">
+        <label for="inputPassword4">Password</label>
+        <input type="password" class="form-control-sm" id="inputPassword4">
+      </div>
+      <div class="form-group col-xs-3 col-sm-4 col-md-6 bg-warning">
+        <label for="inputPassword4">Password</label>
+        <input type="password" class="form-control" id="inputPassword4">
+      </div>
+    </div> 
+  </form>  
+</div> 
+        </div> 
+        </body>
+        </html>
