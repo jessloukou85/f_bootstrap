81917486fe7769b72c7ba6815327147594c41cc5
loukou jess <loukoujess85@gmail.com>
2022-08-29 11:24:38 +0100

    fin de ma premiere page web

--- page.php
+++ page.php
@@ -4,7 +4,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <!-- font awesome --->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <!-- Bootstrap CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <!-- Mon fichier personnel css -->
@@ -13,18 +14,381 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!!</h1>
+  <nav class="band navbar navbar-expand-lg navbar-dark position-fixed ">
+  <div class="container-fluid">
+    <a class="navbar-brand redressed text-uppercase py-3 text-bolder" href="#">Lorem ipsum</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link mx-4" aria-current="page" href="#">Acceuil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link mx-4" href="#">Decouvrir</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link mx-4" href="#">Les classiques</a>
+        </li>
+        <li class="nav-item">
+          <a class="btn btn-order mx-4"href="#">Passer la commande</a>
+        </li>
+      </ul>
+    </div>
+  </div> 
+  </nav>
 
-    <!-- Optional JavaScript; choose one of the two! -->
+    <section class="banner pt-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6"></div>
+          <div class="col-md-6">
+            <h3 class="merriweather text-light">Lorem ipsum dolor sit amet, adipisicing <br>
+             elit. Molestiae sequi vitae praesentium.</h3>
+            <p>Lorem ipsum dolor sit amet consectetur <br>
+             adipisicing elit. Quis, mollitia ea. Illo repellendus 
+            <br> similique impedit, suscipit rem earum omnis optio aliquid <br>
+             atque, quaerat voluptatem sint? Aspernatur laborum suscipit <br> perspiciatis libero. <br>
+              <button class="btn btn-order rounded-0">Commander maintenant</button>
+              <button class="btn btn-info rounded-0">Reserver</button>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <div></div>
+    <section class="page py-2">
+      <div class="container">
+        <div class="row">
+          <div class="card my-1 border-0">
+          <div class="row g-0">
+            <div class="col-md-6">
+              <img src="assets/image/hors-doeuvre.jpg" class="img-fluid rounded max-width: 400px heigth 500px" alt="...">
+            </div>
+            <div class="col-md-6">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><a class="text-muted btn">Last updated 3 mins ago</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+          <div class="card my-1 border-0">
+          <div class="row g-0">
+            <div class="col-md-6">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><a class="text-muted btn">Last updated 3 mins ago</a></p>
+              </div>
+            </div>
+            <div class="col-md-6">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="assets/image/carousel/beans.jpg" class="d-block w-100 rounded-end" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/image/carousel/pizza.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/image/carousel/scotch.jpg" class="d-block w-100" alt="...">
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+            </div>
+          </div>
+          </div>
+          <div class="card my-1 border-0">
+          <div class="row g-0">
+            <div class="col-md-6">
+              <img src="assets/image/drinks.jpg" class="img-fluid rounded max-width: 400px heigth 500px" alt="...">
+            </div>
+            <div class="col-md-6">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><a class="text-muted btn">Last updated 3 mins ago</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
+    <section class="cc-menu py-4 text-muted">
+      <div class="container">
+        <div class="row">
+        <div class="card text-center bg-transparent border-0">
+          <div class="card-header merriweather border-0">
+            <ul class="nav nav-pills card-header-pills justify-content-center">
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">Active</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        <div class="card-body text-light redressed">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+        </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+          <div class="col">
+            <div class="card">
+              <img src="assets/image/breakfast.jpg" class="card-img-top max" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="assets/image/wine-glasses.jpg" class="card-img-top max" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="assets/image/wine.jpg" class="card-img-top max" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="assets/image/coffee.jpg" class="card-img-top max" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="cc-caroussel bg-dark text-light py-4 merriweather">
+      <div class="container">
+        <div class="row">
+          <h1 class="text-uppercase text-center">la nouvelle vision</h1>
+          <div class="col py-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem culpa impedit, modi id repudiandae dignissimos perferendis ex tenetur eius debitis dolores et, soluta reiciendis porro, animi ducimus asperiores minima ab!</div>
+        </div>
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="assets/image/coffee.jpg" class="d-block w-100" alt="...">
+               <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+               </div>
+            </div>
+            <div class="carousel-item">
+              <img src="assets/image/breakfast.jpg" class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="assets/image/carousel/pizza1.jpg" class="d-block w-100" alt="...">
+               <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+               </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    </section>
+
+    <section class="order-form py-5">
+      <div class="container">
+        <h2 class="merriweather mb-4 text-center text-light">passer la commande</h2>
+        <form action="#">
+          <div class="row">
+            <div class="col-md-6 sm">
+              <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1"><i class="fas fa-user"></i></span>
+                <input type="text" class="form-control" placeholder="votre nom">
+              </div>
+              <div class="input-group mb-3">
+                <label class="input-group-text" for="inputGroupSelect01"><i class="fas fa-utesils"></i></label>
+                <select class="form-select" id="inputGroupSelect01">
+                  <option selected>Choisir le Menu</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div class="input-group mb-3">
+                <label class="input-group-text" for="inputGroupSelect01"><i class="fas fa-sort-numeric-up-alt"></i></label>
+                <select class="form-select" id="inputGroupSelect01">
+                  <option selected>Choisir le nombre de persones</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+            </div>
+            <div class="col-md-6 sm">
+              <div class="input-group mb-3">
+                <input type="email" class="form-control" placeholder="votre adresse mail">
+                <span class="input-group-text" id="basic-addon1"><i class="fas fa-at"></i></span>
+              </div>
+              <div class="input-group mb-3">
+                <input type="number" class="form-control" placeholder="">
+                <span class="input-group-text" id="basic-addon1"><i class="fas fa-phone"></i></span>
+              </div>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="adresse zone ex: cocody riviera 2">
+                <span class="input-group-text" id="basic-addon1"><i class="fas fa-home"></i></span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+              <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">preciser le nombre de personnes</span>
+                <input type="text" class="form-control" placeholder="">
+              </div>
+              <div class="input-group">
+                <span class="input-group-text">autres informations</span>
+                <textarea class="form-control" aria-label="With textarea"></textarea>
+              </div>
+          </div>
+        </form>
+      </div>
+    </section>
+
+    <section class="cc-icons">
+      <div class="container">
+        <div class="row">
+        <i class="fa fa-user"></i>
+        <i class="fa fa-book"></i>
+        <i class="fa fa-at"></i>
+        <i class="fa fa-signal"></i>
+        <i class="fa fa-phone"></i>
+        <i class="fa fa-pencil"></i>
+        <i class="fa fa-paper-plane"></i>
+        <i class="fa fa-newspaper-o"></i>
+        <i class="fa fa-music"></i>
+        <i class="fa fa-plane"></i>
+        <i class="fa fa-photo"></i>
+        </div>
+      </div>
+    </section>
+
+    <section class="cc-footer py-5 merriweather">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 sm">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link text-dark"><i class="fa fa-plus"></i> Addition</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa fa-minus"></i> Soustration</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa-solid fa-xmark"></i> Multiplication</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa fa-divide"></i> Division</a>
+            </li>
+          </ul>
+          </div>
+          <div class="col-md-3 sm">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link text-dark"><i class="fa fa-plus"></i> Addition</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa fa-minus"></i> Soustration</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa-solid fa-xmark"></i> Multiplication</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa fa-divide"></i>Division</a>
+            </li>
+          </ul>
+          </div>
+          <div class="col-md-3 sm">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link text-dark active"><i class="fa fa-plus"></i> Addition</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa fa-minus"></i> Soustration</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa-solid fa-xmark"></i> Multiplication</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa fa-divide"></i>Division</a>
+            </li>
+          </ul>
+          </div>
+          <div class="col-md-3 sm">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link text-dark active"><i class="fa fa-plus"></i> Addition</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa fa-minus"></i> Soustration</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa-solid fa-xmark"></i> Multiplication</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#"><i class="fa fa-divide"></i>Division</a>
+            </li>
+          </ul>
+          </div>
+          <div class="copyrigth text-center merriweather py-3 text-muted">
+            <span>copyrigth for jess loukou 29 Aout 2022</span>
+          </div>
+        </div>
+      </div>
+    </section>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
